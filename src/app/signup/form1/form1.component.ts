8c5f635c4595351ef47ddbd96ffed671d9e8c80c
Baher Hossam <Baher1088@gmail.com>
2025-02-25 21:14:33 +0200

    added validation as a json text

--- src/app/signup/form1/form1.component.ts
+++ src/app/signup/form1/form1.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Form2Component } from "../form2/form2.component";
-import {FormsModule} from '@angular/forms';
+// import { Form2Component } from "../form2/form2.component";
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-form1',
-  imports: [FormsModule,Form2Component],
+  imports: [FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './form1.component.html',
   styleUrl: './form1.component.scss'
 })
 
 export class Form1Component {
+
+
+  signupForm1 = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(4)])
+  });
 
 
   constructor(private _router: Router) { }
--- src/app/signup/form2/form2.component.ts
+++ src/app/signup/form2/form2.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-form2',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './form2.component.html',
   styleUrl: './form2.component.scss'
 })
 export class Form2Component {
+
+  signupForm2 = new FormGroup({
+    phoneNum: new FormControl(null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]),
+    nationalId: new FormControl(null, [Validators.required,  Validators.minLength(14), Validators.maxLength(14)])
+  });
 
 
   formData = {
